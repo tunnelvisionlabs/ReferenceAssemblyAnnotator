8abd50888b7db72860af3eee74974a1318b83f75
Sam Harwell <sam@tunnelvisionlabs.com>
2019-08-21 23:34:44 -0700

    Add stub build task

---
+++ TunnelVisionLabs.ReferenceAssemblyAnnotator/AnnotatorBuildTask.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace TunnelVisionLabs.ReferenceAssemblyAnnotator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Resources;
+    using System.Text;
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+
+    public sealed class AnnotatorBuildTask : Task
+    {
+        [Required]
+        public ITaskItem[] ReferenceAssemblies
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string AnnotatedReferenceAssemblyDirectory
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string OutputPath
+        {
+            get;
+            set;
+        }
+
+        [Output]
+        public ITaskItem[] GeneratedAssemblies
+        {
+            get;
+            set;
+        }
+
+        public override bool Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
