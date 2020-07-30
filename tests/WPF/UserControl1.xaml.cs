65deefddcb1efbf39f53920e8d038fc521f59c9d
jnm2 <me@jnm2.com>
2020-07-29 21:12:11 -0400

    Failing tests for using nullability attributes in WPF codebehind

---
+++ tests/WPF/UserControl1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics.CodeAnalysis;
+using System.Windows.Controls;
+
+namespace WPF
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        public static void M(SqlConnection connection, [AllowNull] string? x)
+        {
+            if (!string.IsNullOrEmpty(x))
+            {
+                x.ToString();
+            }
+
+            _ = EqualityComparer<string?>.Default.GetHashCode(x);
+        }
+    }
+}
